1ab402439babe74c06f03f8bad5a42dee44dea97
Hristo Kamenov <hkamenov123@gmail.com>
2023-02-23 12:53:34 +0100

    Some cleanup

--- src/demo/run.rs
+++ src/demo/run.rs
@@ -81,6 +81,7 @@ pub fn run_node(args: NodeArgs) -> Result<()> {
 
     log::info!("generating node's keypair");
     let suite = SuiteEd25519::new_blake3_sha256_ed25519();
+
     let mut save_data = match SaveData::load() {
         Ok(v) => v,
         Err(e) => {
@@ -92,68 +93,10 @@ pub fn run_node(args: NodeArgs) -> Result<()> {
         }
     };
 
-    let keypair = match save_data.node_state {
-        Some(ref node_state) => {
-            log::info!("Loaded keypair");
-            Pair {
-                private: node_state.private_key.clone(),
-                public: node_state.public_key.clone(),
-            }
-        }
-        None => {
-            let pair = new_key_pair(&suite)?;
-            log::info!("Created new keypair");
-            save_data.node_state = Some(NodeState {
-                private_key: pair.private.clone(),
-                public_key: pair.public.clone(),
-                did_document: None,
-            });
-            pair
-        }
-    };
-    match &save_data.save() {
-        Ok(_) => (),
-        Err(e) => log::warn!("{}", e),
-    };
-
+    let keypair = get_keypair(&mut save_data, suite)?;
     let network =
         Network::from_str(&args.network).map_err(|_| anyhow::Error::msg("invalid network"))?;
-
-    let eddsa = EdDSA::from(keypair.clone());
-
-    let did_url = match save_data {
-        SaveData {
-            node_state:
-                Some(NodeState {
-                    did_document: Some(document),
-                    ..
-                }),
-            ..
-        } => {
-            log::info!("Using existing node DID");
-            document.did_url()
-        }
-        _ => {
-            log::info!("creating node's DID document on network {}", args.network);
-            let document = new_document(&eddsa.public.marshal_binary()?, &network, None, None)?;
-            let signature = eddsa.sign(&document.to_bytes()?)?;
-
-            let did_url = document.did_url();
-            document
-                .clone()
-                .publish(&signature, args.node_url.clone())?;
-            log::info!("node's DID document has been published: {}", did_url);
-
-            if let Some(ref mut node_state) = save_data.node_state {
-                node_state.did_document = Some(document);
-            }
-            if let Err(e) = save_data.save() {
-                log::warn!("{}", e);
-            }
-
-            did_url
-        }
-    };
+    let did_url = get_did_url(&keypair, &network, &args.node_url, &mut save_data)?;
 
     let is_completed = Arc::new(AtomicBool::new(false));
 
@@ -259,6 +202,74 @@ pub fn run_node(args: NodeArgs) -> Result<()> {
     sign_listen_relay_handle.join().unwrap()?;
 
     Ok(())
+}
+
+fn get_keypair(
+    save_data: &mut SaveData,
+    suite: SuiteEd25519,
+) -> Result<Pair<kyber_rs::group::edwards25519::Point>, anyhow::Error> {
+    let keypair = match save_data.node_state {
+        Some(ref node_state) => {
+            log::info!("Loaded keypair");
+            Pair {
+                private: node_state.private_key.clone(),
+                public: node_state.public_key.clone(),
+            }
+        }
+        None => {
+            let pair = new_key_pair(&suite)?;
+            log::info!("Created new keypair");
+            save_data.node_state = Some(NodeState {
+                private_key: pair.private.clone(),
+                public_key: pair.public.clone(),
+                did_document: None,
+            });
+            pair
+        }
+    };
+    if let Err(e) = &save_data.save() {
+        log::warn!("{}", e);
+    };
+    Ok(keypair)
+}
+
+fn get_did_url(
+    keypair: &Pair<kyber_rs::group::edwards25519::Point>,
+    network: &Network,
+    node_url: &Option<String>,
+    save_data: &mut SaveData,
+) -> Result<String, anyhow::Error> {
+    let eddsa = EdDSA::from(keypair.clone());
+    let did_url = match &mut save_data.node_state {
+        Some(NodeState {
+            did_document: Some(document),
+            ..
+        }) => {
+            log::info!("Using existing node DID");
+            document.did_url()
+        }
+        _ => {
+            log::info!(
+                "creating node's DID document on network {}",
+                network.to_string()
+            );
+            let document = new_document(&eddsa.public.marshal_binary()?, network, None, None)?;
+            let signature = eddsa.sign(&document.to_bytes()?)?;
+
+            let did_url = document.did_url();
+            document.clone().publish(&signature, node_url.clone())?;
+            log::info!("node's DID document has been published: {}", did_url);
+
+            if let Some(ref mut node_state) = save_data.node_state {
+                node_state.did_document = Some(document);
+            }
+            if let Err(e) = save_data.save() {
+                log::warn!("{}", e);
+            }
+            did_url
+        }
+    };
+    Ok(did_url)
 }
 
 #[derive(Clone, Serialize, Deserialize)]
